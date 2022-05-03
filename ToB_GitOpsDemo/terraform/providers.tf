8bb4278c47bba3c3564c95f4923aa4a10cf23ff9
Damon DeSonier <ddesonier@microsoft.com>
2022-05-03 17:19:09 -0400

    Remove junk

--- ToB_GitOpsDemo/terraform/providers.tf
+++ ToB_GitOpsDemo/terraform/providers.tf
@@ -10,7 +10,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "ToB_Demo"
-    storage_account_name = "tobdemoeastusmay2022a"
+    storage_account_name = "tobdemoeastusmay2022"
     container_name       = "tfstate"
     key                  = "codelab.microsoft.tfstate"
   }
